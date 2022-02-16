37307240c62d31fee923420a5fe4386993ce3925
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 20:41:40 +0200

    style the info box

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -5,8 +5,8 @@
         <div class="tile is-vertical">
           <h2 class="font-size-24px mb-6">About us</h2>
           <p>The occupational traffic permit is one of the most important things in the company when carrying out freight transport.  In fact, it is a prerequisite for doing business traffic at all. </p>
-          <div class="info-box">
-            How do you do when you need to obtain a commercial traffic permit for freight transport to your business?
+          <div class="info-box mt-6">
+            <p class="font-weight-700">How do you do when you need to obtain a commercial traffic permit for freight transport to your business?</p>
           </div>
         </div>
       </div>
