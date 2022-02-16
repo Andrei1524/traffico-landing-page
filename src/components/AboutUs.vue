48bc12bf58d8705972ceb66475bcf64ca0dc4d25
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 20:34:37 +0200

    add about us row + info box

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,7 +1,15 @@
 <template>
   <section class="about-us position-relative">
     <div class="container">
-
+      <div class="about-us-row">
+        <div class="tile is-vertical">
+          <h2 class="font-size-24px mb-6">About us</h2>
+          <p>The occupational traffic permit is one of the most important things in the company when carrying out freight transport.  In fact, it is a prerequisite for doing business traffic at all. </p>
+          <div class="info-box">
+            How do you do when you need to obtain a commercial traffic permit for freight transport to your business?
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
