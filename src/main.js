b2754788daea89c2b5353b91e70c4a4d9d553338
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 18:44:29 +0200

    remove vue-bootstrap, add bulma + header

--- src/main.js
+++ src/main.js
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
-import { BootstrapVue } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/main.scss'
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Homepage from '../views/Homepage.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Homepage
   }
 ]
 
