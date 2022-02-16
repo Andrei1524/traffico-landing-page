480b8280e3c4b12d37046da5846ffa02dac9a783
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 22:49:55 +0200

    Revert "prettify some components"
    This reverts commit 360afe80994735ab18f5929f7e9a576a0dfeff27.

--- src/components/FaqCollapse.vue
+++ src/components/FaqCollapse.vue
@@ -29,9 +29,7 @@
         </div>
       </div>
     </b-collapse>
-    <b-button type="is-primary" class="load-more-btn height-90" expanded>
-      <span class="default-text-base font-weight-700">LOAD MORE</span>
-    </b-button>
+    <b-button type="is-primary" class="load-more-btn height-90" expanded><span class="default-text-base font-weight-700">LOAD MORE</span></b-button>
   </section>
 </template>
 
