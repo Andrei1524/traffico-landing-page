7dabc8e0055a277b54a972637ec3343dfbd9713d
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 20:21:19 +0200

    add about us component

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,48 +1,16 @@
 <template>
-  <section class="top position-relative">
-    <img src="@/assets/images/landing-scene-cropped.svg" alt="" class="landing-scene-image">
-    <Header />
+  <section class="about-us position-relative">
     <div class="container">
-      <div class="intro position-relative">
-        <h1 class="font-size-100px default-text is-size-3-mobile">Your awesome traffic permit consultant.</h1>
-        <b-button type="is-primary" class="default-btn font-weight-700 mt-6">GET STARTED</b-button>
-      </div>
+
     </div>
   </section>
 </template>
 
 <script>
-import Header from '@/components/Header'
-
 export default {
-  name: 'Top',
-  components: {
-    Header
-  }
+  name: 'Top'
 }
 </script>
 
 <style lang="scss" scoped>
-section {
-  height: 100vh;
-}
-
-.intro {
-  padding-top: 90px;
-  max-width: 741px;
-
-  h1 {
-    font-family: 'DM Serif Display', serif;
-    line-height: 100%;
-  }
-}
-
-@media only screen and (max-width: 600px) {
-  .intro {
-    position: absolute !important;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-}
-
 </style>
