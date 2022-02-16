21aab3b067e50a126c58de6a1a5c1f5b1041136a
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 22:50:11 +0200

    Revert "Revert "prettify some components""
    This reverts commit 480b8280e3c4b12d37046da5846ffa02dac9a783.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
     <Top />
-    <img v-animate-css="'slideInLeft'" src="@/assets/images/landing-truck.svg" alt="" class="truck position-relative">
-    <AboutUs/>
+    <img
+      v-animate-css="'slideInLeft'"
+      src="@/assets/images/landing-truck.svg"
+      alt=""
+      class="truck position-relative"
+    />
+    <AboutUs />
     <Faq />
   </div>
 </template>
 
 <script>
-import Top from '@/components/Top'
-import AboutUs from '@/components/AboutUs'
-import Faq from '@/components/Faq'
+import Top from "@/components/Top";
+import AboutUs from "@/components/AboutUs";
+import Faq from "@/components/Faq";
 
 export default {
-  components: { Top, AboutUs, Faq }
-}
+  components: { Top, AboutUs, Faq },
+};
 </script>
 
 <style lang="scss">
