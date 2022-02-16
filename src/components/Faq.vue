e3cd67c2a37c92b2bf411ea282cae1a087aaa1c0
Mandrican Andrei <adyta094@gmail.com>
2022-02-16 21:28:04 +0200

    styled the faq description

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,6 +1,15 @@
 <template>
-  <section class="about-us position-relative">
+  <section class="faq position-relative">
     <div class="container">
+      <div class="columns is-justify-content-space-between">
+        <div class="column is-half">
+          <h2 class="default-text-base font-weight-700 font-size-24px mb-6">Faq</h2>
+          <p class="font-family-dm font-size-48px default-text-brown">Questions and Answers on Professional Traffic Permits:</p>
+        </div>
+        <div class="column is-half">
+          <img src="@/assets/images/landing-figure-faq.png" alt="" class="figure-1" v-animate-css="'slideInRight'">
+        </div>
+      </div>
     </div>
   </section>
 </template>
