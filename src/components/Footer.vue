edfc9d5086ec121fc47adff42a84c70d21f8cd6e
Mandrican Andrei <adyta094@gmail.com>
2022-02-17 12:11:01 +0200

    add footer truck

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,13 @@
 <template>
   <section class="footer position-relative">
+    <img
+      src="@/assets/images/footer-truckmobil.png"
+      alt=""
+      class="truck position-relative"
+      data-aos="fade-right"
+      data-aos-offset="300"
+      data-aos-easing="ease-in-sine"
+    >
     <div class="container">
       footer
     </div>
@@ -21,5 +29,11 @@ section {
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+}
+
+.truck {
+  position: absolute;
+  top: -200px;
+  left: 0;
 }
 </style>
